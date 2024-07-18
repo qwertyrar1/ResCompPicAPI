6a2bf4660ce797aef364d25f7ceb7658682fdfd1
qwertyrar1 <tyrsinartem@gmail.com>
2024-07-18 21:45:20 +0500

    add requirements.txt

--- settings.py
+++ settings.py
@@ -3,6 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 WATERMARK_PATH = os.getenv("WATERMARK_PATH")
 IMAGE_FORMATS = ['jpeg', 'png', 'bmp', 'gif']
